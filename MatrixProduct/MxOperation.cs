8ea43330ad6b009f3c681474235a12e6be6f274e
kendoka <ncrihan@gmail.com>
2019-02-28 18:18:51 -0500

    LoadData in parallel

--- MatrixProduct/MxOperation.cs
+++ MatrixProduct/MxOperation.cs
@@ -1,8 +1,10 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,6 @@ namespace MatrixProduct
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private readonly InvCloudService cService;
         private readonly int size;
-        //private readonly long[,] A;//private readonly long[,] B;
         private Matrix<double> A;
         private Matrix<double> B;
         private double[,] C;
@@ -29,18 +30,27 @@ namespace MatrixProduct
 
         public void LoadData()
         {
-            log.Info($"LoadData starting.");
-            var rowsA = new List<double[]>();
-            var rowsB = new List<double[]>();
+            log.Info($"LoadData started.");
+            var rowsA = new ConcurrentDictionary<int, double[]>();
+            var rowsB = new ConcurrentDictionary<int, double[]>();
 
-            for (int i = 0; i < size; i++)
-            {
-                rowsA.Add(cService.GetRowData("A", i));
-                rowsB.Add(cService.GetRowData("B", i));
-            }
+            var result = Parallel.For(0, size, (i, state) => {
 
-            A = DenseMatrix.OfRowArrays(rowsA);
-            B = DenseMatrix.OfRowArrays(rowsB);
+                if (state.ShouldExitCurrentIteration)
+                {
+                    if (state.LowestBreakIteration < i)
+                        return;
+                }
+
+                rowsA[i] = cService.GetRowData("A", i);
+                rowsB[i] = cService.GetRowData("B", i);
+            });
+
+            var da = rowsA.OrderBy(x => x.Key).ToDictionary((keyItem) => keyItem.Key, (valueItem) => valueItem.Value).Values.ToArray();
+            var db  = rowsB.OrderBy(x => x.Key).ToDictionary((keyItem) => keyItem.Key, (valueItem) => valueItem.Value).Values.ToArray();
+
+            A = DenseMatrix.OfRowArrays(da);
+            B = DenseMatrix.OfRowArrays(db);
 
             log.Info($"LoadData complete.");
         }
@@ -54,11 +64,14 @@ namespace MatrixProduct
 
         public void Validate()
         {
-            log.Info($"Validating hash.");
-            var strf = Formated(C);
+            log.Info($"Compute hash started.");
+            var strf = FormatM(C);
             var hs = MD5Hash(strf);
+            log.Info($"Hash compute complete.");
+
+            log.Info($"Validating hash.");
             var resp = cService.Validate(hs);
-            log.Info($"Validation response: {nameof(resp.Success)} : {resp.Success}");
+            log.Info($"Validation response: {resp.Value}. {nameof(resp.Success)}: {resp.Success}");
         }
 
         private long[,] mProduct(int[,] A, int[,] B)
@@ -82,20 +95,36 @@ namespace MatrixProduct
             return C;
         }
 
-        //MD5 hash of values. Joined by colulmn then by row into a single string without separators and then hashed.
-        private string Formated(double[,] A)
+        //Joined by colulmn then by row into a single string without separators and then hashed.
+        private string FormatM(double[,] A)
         {
             var retval = string.Empty;
-            var size = A.GetLength(0);
+            var matrixFormat = new ConcurrentDictionary<int, string>();
+            var result = Parallel.For(0, size, (i, state) => {
 
-            foreach (var r in A)
-                retval += r.ToString();
+                if (state.ShouldExitCurrentIteration)
+                {
+                    if (state.LowestBreakIteration < i)
+                        return;
+                }
 
-            //for (int i = 0; i < size; i++)
-            //    for (int j = 0; j < size; j++)
-            //        retval += A[j, i].ToString();
+                var row = GetRow(i);
+                var newSet = string.Join(null, row.ToList());
+                var res = matrixFormat.TryAdd(i, newSet);
+            });
 
+            retval = string.Join(null, matrixFormat.OrderBy(x => x.Key).Select(y => y.Value));
             return retval;
+        }
+
+        private double[] GetRow(int row)
+        {
+            int cols = C.GetUpperBound(1) + 1;
+            double[] result = new double[cols];
+
+            int size = Marshal.SizeOf<double>();
+            Buffer.BlockCopy(C, row * cols * size, result, 0, cols * size);
+            return result;
         }
 
         private string MD5Hash(string text)
